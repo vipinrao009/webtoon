2cf6d1240b84c5af50b6e4fc8017f746dc35e9ce
vipinrao009 <vipinrao2018@gmail.com>
2024-10-10 07:47:33 +0530

    deploying the project....

--- server/server.js
+++ server/server.js
@@ -5,8 +5,11 @@ import express from "express";
 import connectDB from "./config/db.connection.js";
 import webtoon from "./routes/webtoonRoutes.js";
 import cloudinary from "cloudinary";
+import path from "path"
 
 const app = express();
+const _dirname = path.resolve();
+
 dotenv.config()
 
 connectDB();
@@ -29,6 +32,11 @@ app.use(express.json());
 
 app.use('/api/v1/webtoon',webtoon)
 
+app.use(express.static(path.join(_dirname,"/client/build")))
+
+app.get('*',(req,res)=>{
+  res.sendFile(path.resolve(_dirname,"client","build","index.html"))
+})
 // Test Route
 app.get("/", (req, res) => {
   res.send("Backend is working!");
